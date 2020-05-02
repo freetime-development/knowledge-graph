a33f0ed9579db9797419eb74784a1b6a7a5e7aae
Evan Santa <evan.santa@treeti.com>
2020-05-02 22:56:20 +0200

    Basic dgraph import

---
+++ database/dgraph_import.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+container_name=${container_name:-alpha1}
+data_file=${data_file:-g01.rdf.gz}
+schema_file=${schema_file:-g01.schema.gz}
+
+while [ $# -gt 0 ]; do
+
+   if [[ $1 == *"--"* ]]; then
+        param="${1/--/}"
+        declare $param="$2"
+   fi
+
+  shift
+done
+echo $schema_file
+docker exec -it $container_name dgraph bulk -f $data_file -s $schema_file --map_shards=1 --reduce_shards=1 --http localhost:8001 --zero=zero:5080 
+docker exec -it $container_name rm -rf /dgraph/p
+docker exec -it $container_name mv /dgraph/out/0/p /dgraph/p
+docker exec -it $container_name rm -rf /dgraph/out
