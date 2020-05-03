8f4e568feb1573cedf057f830fa55802c9380dcc
Michal <michal@omg.sk>
2020-05-03 17:08:37 +0200

    added to_load source folder

--- database/dgraph_import.sh
+++ database/dgraph_import.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 container_name=${container_name:-alpha1}
-data_file=${data_file:-/dgraph/g01.rdf.gz}
-schema_file=${schema_file:-/dgraph/g01.schema.gz}
+data_file=${data_file:-/dgraph/to_load/g01.rdf.gz}
+schema_file=${schema_file:-/dgraph/to_load/g01.schema.gz}
 
 while [ $# -gt 0 ]; do
 
@@ -13,12 +13,16 @@ while [ $# -gt 0 ]; do
 
   shift
 done
-if [ ! -d server1 ]; then
-    mkdir server1
+
+if [ ! -d ./server1/to_load ]; then
+    mkdir ./server1/to_load
 fi
-cp ${data_file/\/dgraph\//} server1/ && cp ${schema_file/\/dgraph\//} server1/
+
+cp ${data_file/\/dgraph\/to_load\//} server1/to_load/ && cp ${schema_file/\/dgraph\/to_load\//} server1/to_load/
+
 docker exec -it $container_name dgraph bulk -f $data_file -s $schema_file --map_shards=1 --reduce_shards=1 --http localhost:8001 --zero=zero:5080 
 docker exec -it $container_name rm -rf /dgraph/p
 docker exec -it $container_name mv /dgraph/out/0/p /dgraph/p
 docker exec -it $container_name rm -rf /dgraph/out
-rm server1/g01*
+
+rm server1/to_load/g01*
