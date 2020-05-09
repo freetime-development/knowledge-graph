f55dcfcd5cc34ef93c08f782bf0e76e9192344b3
Michal <michal@omg.sk>
2020-05-09 22:00:01 +0200

    added to_load local folder

--- database/dgraph_import.sh
+++ database/dgraph_import.sh
@@ -18,7 +18,7 @@ if [ ! -d ./server1/to_load ]; then
     mkdir ./server1/to_load
 fi
 
-cp ${data_file/\/dgraph\/to_load\//} server1/to_load/ && cp ${schema_file/\/dgraph\/to_load\//} server1/to_load/
+cp ${data_file/\/dgraph\//} server1/to_load/ && cp ${schema_file/\/dgraph\//} server1/to_load/
 
 docker exec -it $container_name dgraph bulk -f $data_file -s $schema_file --map_shards=1 --reduce_shards=1 --http localhost:8001 --zero=zero:5080 
 docker exec -it $container_name rm -rf /dgraph/p
