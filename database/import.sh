fdb5c07f145cadb31b02428929fa8d0620c5899f
Evan Santa <code.Selftaught@gmail.com>
2020-05-11 20:34:22 +0200

    Import (#6)
    * Basic dgraph import
    
    * modified seed paths
    
    * modified seeds handling in dgraph_import
    
    * added to_load source folder
    
    * added to_load local folder
    
    * Adding files
    
    Co-authored-by: Evan Santa <evan.santa@treeti.com>
    Co-authored-by: Michal <michal@omg.sk>

---
+++ database/import.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+container_name=${container_name:-alpha1}
+data_file=${data_file:-/dgraph/import/g01.rdf.gz}
+schema_file=${schema_file:-/dgraph/import/g01.schema.gz}
+
+while [ $# -gt 0 ]; do
+
+   if [[ $1 == *"--"* ]]; then
+        param="${1/--/}"
+        declare $param="$2"
+   fi
+
+  shift
+done
+
+if [ ! -d ./server1/import ]; then
+    mkdir ./server1/import
+fi
+
+cp ${data_file/\/dgraph\//} server1/import/ && cp ${schema_file/\/dgraph\//} server1/import/
+
+docker exec -it $container_name dgraph bulk -f $data_file -s $schema_file --map_shards=1 --reduce_shards=1 --http localhost:8001 --zero=zero:5080 
+docker exec -it $container_name rm -rf /dgraph/p
+docker exec -it $container_name mv /dgraph/out/0/p /dgraph/p
+docker exec -it $container_name rm -rf /dgraph/out
+
+rm server1/import/g01*
