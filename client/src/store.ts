2689f8bb2d423ab7c85206cc75fbe41070f46e70
Evan Santa <evan.santa@treeti.com>
2021-04-04 20:57:36 +0200

    Removed unused code

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,25 +1,18 @@
-import { Store } from '@material-ui/icons'
 import React from 'react'
 import NodeController from './controllers/nodeController'
 import NoteController from './controllers/noteController'
 import NodeService from './services/nodeService'
 import NoTeService from './services/noteService'
-import TranslationService from './services/translationService'
 import NodeStore from './stores/nodeStore'
 import NoteStore from './stores/noteStore'
-import TranslationStore from './stores/translationStore'
 
 interface Store {
-  // translationStore: TranslationStore
 }
 
 interface ControllerContext {
   nodeController: NodeController
   noteController: NoteController
 }
-
-const translationService = new TranslationService()
-// const translationStore = new TranslationStore(translationService)
 
 const nodeStore = new NodeStore()
 const nodeService = new NodeService()
@@ -30,7 +23,6 @@ const noteService = new NoTeService()
 const noteController = new NoteController(noteStore, noteService)
 
 export const storesContext = React.createContext<Store>({
-  // translationStore,
 })
 
 const controllersContext = React.createContext<ControllerContext>({
--- client/src/utils/alert.ts
+++ client/src/utils/alert.ts
@@ -1,4 +1,4 @@
-import { TranslationKey } from '../stores/translationStore'
+// import { TranslationKey } from '../stores/translationStore'
 
 export enum Severity {
   ERROR = 'error',
@@ -8,7 +8,7 @@ export enum Severity {
 }
 
 export interface AlertMessage {
-  message: TranslationKey // 1i8n key
+  // message: TranslationKey // 1i8n key
   severity: Severity
   params?: object // 1i8n params
 }
